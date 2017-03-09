1cab85ec4df301727c37e854ec23a2bbc9986789
Technolords <mike@technolords.net>
2017-03-09 12:20:18 +0100

    Added shutdown hook, as well as joining current thread to prevent immediate termination

--- grizzly2-resource-annotated/src/main/java/net/technolords/benchmark/server/JerseyMain.java
+++ grizzly2-resource-annotated/src/main/java/net/technolords/benchmark/server/JerseyMain.java
@@ -12,12 +12,21 @@ import com.sun.jersey.api.core.ClasspathResourceConfig;
 public class JerseyMain {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
-    public void configureAndRun() throws IOException {
+    public void configureAndRun() throws IOException, InterruptedException {
         LOGGER.info("Starting server...");
         String uri = "http://localhost:9090/";
         ClasspathResourceConfig classpathResourceConfig = new ClasspathResourceConfig();
         HttpServer httpServer = GrizzlyServerFactory.createHttpServer(uri, classpathResourceConfig);
         httpServer.start();
+        this.registerShutdownHook(httpServer);
+        Thread.currentThread().join(); // Prevents termination
+    }
+
+    public void registerShutdownHook(HttpServer httpServer) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOGGER.info("Terminating server...");
+            httpServer.stop();
+        }));
     }
 
     public static void main(String[] args) throws Exception {
