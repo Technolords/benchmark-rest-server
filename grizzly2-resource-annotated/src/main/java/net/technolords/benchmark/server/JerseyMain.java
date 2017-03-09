a5b3ae052ae4a5c7173651485e04eccd61f20ffa
Technolords <mike@technolords.net>
2017-03-09 15:01:25 +0100

    Fixed implementation for Grizzly2

--- grizzly2-resource-annotated/src/main/java/net/technolords/benchmark/server/JerseyMain.java
+++ grizzly2-resource-annotated/src/main/java/net/technolords/benchmark/server/JerseyMain.java
@@ -1,31 +1,41 @@
 package net.technolords.benchmark.server;
 
 import java.io.IOException;
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
-import com.sun.jersey.api.core.ClasspathResourceConfig;
 
 public class JerseyMain {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     public void configureAndRun() throws IOException, InterruptedException {
         LOGGER.info("Starting server...");
-        String uri = "http://localhost:9090/";
-        ClasspathResourceConfig classpathResourceConfig = new ClasspathResourceConfig();
-        HttpServer httpServer = GrizzlyServerFactory.createHttpServer(uri, classpathResourceConfig);
+        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(this.createBaseURI(), this.createResourceConfig());
         httpServer.start();
         this.registerShutdownHook(httpServer);
         Thread.currentThread().join(); // Prevents termination
     }
 
+    private URI createBaseURI() {
+        return UriBuilder.fromUri("http://localhost/").port(9090).build();
+    }
+
+    private ResourceConfig createResourceConfig() {
+        ResourceConfig resourceConfig = new ResourceConfig();
+        resourceConfig.packages("net.technolords.benchmark.server.api");
+        return resourceConfig;
+    }
+
     private void registerShutdownHook(HttpServer httpServer) {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             LOGGER.info("Terminating server...");
-            httpServer.stop();
+            httpServer.shutdown();
         }));
     }
 
