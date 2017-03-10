ed3a12deb2a8d95d929a0a1b7adde2629c5f9fbf
Technolords <mike@technolords.net>
2017-03-10 16:09:15 +0100

    Added vertx reactive module

---
+++ vertx3-reactive/src/main/java/net/technolords/benchmark/server/VertxMain.java
@@ -0,0 +1,63 @@
+package net.technolords.benchmark.server;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.VertxOptions;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.Vertx;
+import io.vertx.rxjava.core.http.HttpServer;
+import io.vertx.rxjava.core.http.HttpServerResponse;
+import net.technolords.benchmark.resource.ResourceManager;
+
+// http://vertx.io/docs/vertx-rx/java/
+public class VertxMain extends AbstractVerticle {
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private static String BUFFERED_RESPONSE;
+
+    public void configureAndRun() {
+        LOGGER.info("Starting server...");
+        Consumer<Vertx> runner = vertx1 -> {
+            vertx1.deployVerticle(this.getClass().getName());
+        };
+        Vertx vertx = Vertx.vertx(this.createVertxOptions());
+        runner.accept(vertx);
+    }
+
+    @Override
+    public void start() throws Exception {
+        LOGGER.info("Yay started....");
+        try {
+            BUFFERED_RESPONSE = ResourceManager.getCountriesAsJsonString();
+        } catch (IOException e) {
+            LOGGER.error("Failed to buffer response", e);
+        }
+        HttpServer httpServer = vertx.createHttpServer();
+        httpServer
+                .requestStream()
+                .toObservable()
+                .subscribe(httpServerRequest -> {
+                    HttpServerResponse response = httpServerRequest.response();
+                    response
+                            .setChunked(true)
+                            .putHeader("Content-Type", "application/json")
+                            .end(BUFFERED_RESPONSE);
+                });
+        httpServer.listen(9090);
+    }
+
+    private VertxOptions createVertxOptions() {
+        VertxOptions vertxOptions = new VertxOptions();
+        vertxOptions.setWorkerPoolSize(512);
+        return vertxOptions;
+    }
+
+    public static void main(String[] args) throws Exception {
+        VertxMain vertxMain = new VertxMain();
+        vertxMain.configureAndRun();
+    }
+
+}
