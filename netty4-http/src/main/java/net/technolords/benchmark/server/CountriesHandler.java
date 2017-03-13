5eef255315499d4a0f42ae61e0c31d5f173be348
Technolords <mike@technolords.net>
2017-03-13 14:29:33 +0100

    Fixed UTF-8 issue

--- netty4-http/src/main/java/net/technolords/benchmark/server/CountriesHandler.java
+++ netty4-http/src/main/java/net/technolords/benchmark/server/CountriesHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -14,6 +15,7 @@ import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.CharsetUtil;
 import net.technolords.benchmark.resource.ResourceManager;
 
 public class CountriesHandler extends ChannelInboundHandlerAdapter {
@@ -42,7 +44,8 @@ public class CountriesHandler extends ChannelInboundHandlerAdapter {
         if (msg instanceof HttpRequest) {
             HttpRequest httpRequest = (HttpRequest) msg;
             LOGGER.debug("Request: {}", httpRequest);
-            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer(BUFFERED_RESPONSE.getBytes()));
+            ByteBuf byteBuf = Unpooled.copiedBuffer(BUFFERED_RESPONSE, CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, JSON_CONTENT_TYPE);
             response.headers().setInt(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
             ctx.write(response);
