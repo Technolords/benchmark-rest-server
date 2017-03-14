ffb840558b00ef77f80176e9204df22892616105
Technolords <mike@technolords.net>
2017-03-14 10:59:34 +0100

    Made port and thread pool size configurable

--- grizzly1-servlet-raw/src/main/java/net/technolords/benchmark/server/JerseyMain.java
+++ grizzly1-servlet-raw/src/main/java/net/technolords/benchmark/server/JerseyMain.java
@@ -8,13 +8,17 @@ import org.slf4j.LoggerFactory;
 import com.sun.grizzly.http.SelectorThread;
 import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 
+import net.technolords.benchmark.config.ConfigurationManager;
+
 public class JerseyMain {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     public void configureAndRun() throws IOException {
         LOGGER.info("Starting server...");
-        String uri = "http://localhost:9090/countries";
+        int port = ConfigurationManager.getPort();
+        String uri = "http://localhost:" + port + "/countries";
         SelectorThread selectorThread = GrizzlyWebContainerFactory.create(uri, CountriesServlet.class);
+        selectorThread.setMaxThreads(ConfigurationManager.getPoolSize());
         selectorThread.run();
     }
 
